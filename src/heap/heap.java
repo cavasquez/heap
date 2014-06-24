6eca0149cf7eff8bd670da2110e14661ea249911
Carlos Vasquez <cvasquez@miamiparking.com>
2014-06-24 11:56:57 -0400

    implementing heap

--- src/heap/heap.java
+++ src/heap/heap.java
@@ -7,5 +7,22 @@ package heap;
  */
 public class heap 
 {
+	public static void main(String[] args)
+	{
+		switch(args[0])
+		{
+			case"-r":
+			break;
+			case "-il":
+			break;
+			case "-ib":
+			break;
+			default: System.out.println("Invalid command. Valid commands are: -r, -il {file}, -ib {file}");
+		}
+	}
 	
+	protected void rand()
+	{
+		
+	}
 }
